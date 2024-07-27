1e9ee353edd7d8228b4c74daea44fbfc4c502d72
OriSnow420_ <1315376634@qq.com>
2024-07-27 23:57:41 +0800

    Done with objExporter

--- src/Model/header/objExporter.hpp
+++ src/Model/header/objExporter.hpp
@@ -5,5 +5,14 @@
 #define objExporter_hpp
 
 // TODO: finish the class
+#include "Exporter.hpp"
+#include "Model3D.hpp"
+
+class objExporter : public Exporter {
+private:
+    virtual bool checkExtension(const std::string& path) const override;
+    virtual void writeToFile(const Model3D& model, 
+    const std::string& finename) override;
+};
 
 #endif
---
+++ src/Model/objExporter.cpp
@@ -0,0 +1,35 @@
+#include "header/objExporter.hpp"
+#include "header/Model3D.hpp"
+#include <regex>
+
+bool objExporter::checkExtension(const std::string& path) const {
+    return std::regex_match(path, std::regex("^.*\\.obj$"));
+}
+
+void objExporter::writeToFile(const Model3D& model, 
+const std::string& filename) {
+    getFile() << "# " << filename << std::endl
+              << "g " << model.getName() << std::endl;
+    for (const auto& point : model.getPoints()) {
+        getFile() << "v " << point.getCoordinate(0) << ' '
+                          << point.getCoordinate(1) << ' '
+                          << point.getCoordinate(2) << ' ';
+    }
+    int i = 1;
+    for (const auto& line : model.getLines()) {
+        getFile() << "l " << i << ' ';
+        i++;
+        getFile() << "i" << ' ';
+        i++;
+        getFile() << std::endl;
+    }
+    for (const auto& face : model.getFaces()) {
+        getFile() << "f " << i << ' ';
+        i++;
+        getFile() << i << ' ';
+        i++;
+        getFile() << i << ' ';
+        i++;
+        getFile() << std::endl;
+    }
+}
