97a075b2d3e7485bba06cc74ef6844ffbc6864e4
OriSnow420_ <1315376634@qq.com>
2024-08-06 14:00:29 +0800

    Error Msg changed

--- src/Model/header/MultiPoint.hpp
+++ src/Model/header/MultiPoint.hpp
@@ -14,7 +14,8 @@ private:
     std::vector<Point3D> m_points;
     virtual void innerSet(const Point3D& newPoint, int index){
         if (index < 0 || index >= N){
-            throw std::exception("[MultiPoint::innerSet] Passing wrong number of arguments");
+            throw std::exception(
+                "[MultiPoint::innerSet] Passing wrong number of arguments");
         }
         m_points[index] = newPoint;
     }
@@ -23,13 +24,17 @@ public:
     explicit MultiPoint(std::initializer_list<Point3D> Ilist):
     m_points(Ilist){
         if(Ilist.size() != N){
-            throw std::exception("[MultiPoint::MultiPoint] Passing wrong number of arguments");
+            throw std::exception(
+                "[MultiPoint::MultiPoint] Passing wrong number of arguments");
         }
     }
 
+    virtual ~MultiPoint() {}
+
     bool operator==(const MultiPoint<N>& rhs) const noexcept {
-        for (const auto& point : m_points){
-            if (std::find(rhs.m_points.begin(), rhs.m_points.end(), point) == rhs.m_points.end()) {
+        for (const auto& point : m_points) {
+            if (std::find(rhs.m_points.begin(), rhs.m_points.end(), 
+            point) == rhs.m_points.end()) {
                 return false;
             }
         }
@@ -46,7 +51,8 @@ public:
 
     Point3D getPoint(int index) const {
         if(index < 0 || index >= N){
-            throw std::exception("[MultiPoint::getPoint] Passing wrong index!");
+            throw std::exception(
+                "[MultiPoint::getPoint] Passing wrong index!");
         }
         return m_points[index];
     }
